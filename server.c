b438329f6d69da0b00c6c930f71c6eecf4169d1d
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-30 18:51:38 +0000

    Add server.c

---
+++ server.c
@@ -0,0 +1,76 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <time.h>        
+
+#define SERVER_PORT 1300
+#define BUFSIZE 256
+
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket; 
+    struct sockaddr_in address;
+    
+    int opt = 1;      // for setsockopt() SO_REUSEADDR, below
+    int addrlen = sizeof(address); 
+    char buffer[BUFSIZE];
+
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 1300 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( SERVER_PORT ); 
+       
+    // Bind the socket to the network address and port
+    if (  bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0  ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    while(1) {  // Server loop
+      // Wait for a connection
+      if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                               (socklen_t*)&addrlen))<0) 
+        { 
+          perror("accept failed"); 
+          exit(EXIT_FAILURE); 
+        }
+
+      printf("One client has connected.\n");
+
+      // Get current Time Stuff
+      time_t rawtime;
+      struct tm * timeinfo;
+      time ( &rawtime );
+      timeinfo = localtime ( &rawtime );
+
+      send(new_socket, asctime (timeinfo), strlen(asctime (timeinfo)) , 0);
+      
+      buffer[0] = 0;
+      
+      close(new_socket);
+    }
+    
+    return 0; 
+} 
